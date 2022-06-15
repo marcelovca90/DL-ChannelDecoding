31a8c8092587d7eadb6ae73d9b95c1d92f753a0b
Marcelo Cysneiros <marcelovca90@gmail.com>
2022-06-15 00:34:30 -0300

    add auxiliary scripts

---
+++ scripts/run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+export SCENARIO='scenario-9-sbrt-timed-08M'
+
+cd ../experiments/$SCENARIO/by-epoch && python DL-CD-MAP-GPU-EP.py | tee DL-CD-MAP-GPU-EP.log
+cd -
+cd ../experiments/$SCENARIO/by-topology-4-layers && python DL-CD-MAP-GPU-4L.py | tee DL-CD-MAP-GPU-4L.log
+cd -
+cd ../experiments/$SCENARIO/by-topology-5-layers && python DL-CD-MAP-GPU-5L.py | tee DL-CD-MAP-GPU-5L.log
+cd -
+cd ../experiments/$SCENARIO/by-topology-6-layers && python DL-CD-MAP-GPU-6L.py | tee DL-CD-MAP-GPU-6L.log
